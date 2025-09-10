3d0cb73eb7eab8fbf00a3d1768f87680afe7dfe8
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-10 10:27:07 +0530

    Initial commit

---
+++ even_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num % 2 == 0){
+        printf("%d is an Even number\n",num);
+    } else {
+        printf("%d is an Odd number\n",num);
+    }
+    return 0;
+}
