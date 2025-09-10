ccd6d46ca9c6cdd2e1ee1873a5430b8546bc84ae
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-10 10:26:25 +0530

    Initial commit

---
+++ Area_rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int length, breadth;
+    printf("Enter lenghth:");
+    scanf("%d", &length);
+    printf("Enter breadth:");
+    scanf("%d", &breadth);
+    printf("Area of rectangle is %d", length*breadth);
+    return 0;
+}
