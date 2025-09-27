38327c6ec105e970e77b988b91a88832b56e91b2
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-27 23:29:56 +0530

    4_addnumber.c

--- 4_addnumber.c
+++ 4_addnumber.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int main(){
+int main()
+{
     int num1,num2;
     printf("Enter 1st number : ");
     scanf("%d",&num1);
@@ -8,3 +9,4 @@ int main(){
     printf("The sum of %d and %d is %d\n", num1, num2, num1+num2);
     return 0;
 }
+
