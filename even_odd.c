629162797148bd095289717696bcc965f42e31dc
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-28 23:53:54 +0530

    even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -4,12 +4,14 @@ int main()
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    if(num % 2 == 0){
+    if(num % 2 == 0)
+    {
         printf("%d is an Even number\n",num);
     } else {
         printf("%d is an Odd number\n",num);
     }
     return 0;
 }
+
 
 
