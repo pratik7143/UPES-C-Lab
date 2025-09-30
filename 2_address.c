97cd9a274d1fc8ba563611867cb213e6a4dc4ef6
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-30 23:54:26 +0530

    2_address.c

--- 2_address.c
+++ 2_address.c
@@ -3,7 +3,6 @@ int main()
 
 
 
-
 {
     printf("Plot No. 893/894\n");
     printf("Agarwal's Residence\n");
@@ -12,6 +11,7 @@ int main()
     printf("Pin Code: 751007\n");
     return 0;
 }
+
 
 
 
